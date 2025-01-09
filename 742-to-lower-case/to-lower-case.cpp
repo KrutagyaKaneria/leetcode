8d1cffbeb437706468dd13953f5d4e26d70e72de
krutagya kaneria <krutagya.kaneria.cg@gmail.com>
2025-01-09 09:46:06 +0530

    Time: 0 ms (100.00%) | Memory: 8.1 MB (59.04%) - LeetSync

--- 742-to-lower-case/to-lower-case.cpp
+++ 742-to-lower-case/to-lower-case.cpp
@@ -10,11 +10,27 @@ public:
 //     }
 // };
 
-
- for (int i = 0; i < s.length(); i++) {
-        s[i] = tolower(s[i]);
-    }
+ transform(s.begin(), s.end(), s.begin(), ::tolower);
     return s;
-}
+    }
 };
+
+
+
+
+
+//  for (int i = 0; i < s.length(); i++) {
+//         s[i] = tolower(s[i]);
+//     }
+//     return s;
+// }
+// };
+
+
+
+
+
+
+
+
 
