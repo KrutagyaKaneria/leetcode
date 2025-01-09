0b086606413b916125a0892234553ca31d8cbfea
krutagya kaneria <krutagya.kaneria.cg@gmail.com>
2025-01-09 09:58:24 +0530

    Time: 0 ms (100.00%) | Memory: 27.2 MB (81.90%) - LeetSync

--- 344-reverse-string/reverse-string.cpp
+++ 344-reverse-string/reverse-string.cpp
@@ -1,16 +1,30 @@
+// class Solution {
+// public:
+//     void reverseString(vector<char>& s, int start = 0, int end = -1) {
+// //         if (end == -1) {
+// //             end = s.size() - 1;
+// //         }
+// //         if (start >= end) {
+// //             return;
+// //         }
+// //         swap(s[start], s[end]);
+// //         reverseString(s, start + 1, end - 1);
+// //     }
+// // };
+
+
 class Solution {
 public:
-    void reverseString(vector<char>& s, int start = 0, int end = -1) {
-        if (end == -1) {
-            end = s.size() - 1;
+    void reverseString(vector<char>& s) {
+        int n = s.size();
+
+        // Use a single loop to swap characters from both ends
+        for (int i = 0; i < n / 2; ++i) {
+            swap(s[i], s[n - i - 1]);
         }
-        if (start >= end) {
-            return;
-        }
-        swap(s[start], s[end]);
-        reverseString(s, start + 1, end - 1);
     }
 };
+
 
 
     
