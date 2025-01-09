1a8d908928986de4d8b2c5233981fc6f57e8d881
krutagya kaneria <krutagya.kaneria.cg@gmail.com>
2025-01-09 09:43:22 +0530

    Time: 0 ms (100.00%) | Memory: 8.3 MB (39.22%) - LeetSync

--- 742-to-lower-case/to-lower-case.cpp
+++ 742-to-lower-case/to-lower-case.cpp
@@ -1,15 +1,20 @@
 class Solution {
 public:
     string toLowerCase(string s) {
-        for(int i=0;i<=s.length();i++){
-            if(s[i]>='A' && s[i]<= 'Z'){
-                s[i]=s[i]+32;
-            }
-        }
-        return s;
+//         for(int i=0;i<=s.length();i++){
+//             if(s[i]>='A' && s[i]<= 'Z'){
+//                 s[i]=s[i]+32;
+//             }
+//         }
+//         return s;
+//     }
+// };
+
+
+ for (int i = 0; i < s.length(); i++) {
+        s[i] = tolower(s[i]);
     }
+    return s;
+}
 };
-
-
-
 
