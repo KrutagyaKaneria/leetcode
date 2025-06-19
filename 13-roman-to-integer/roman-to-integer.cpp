40c91a4ed98883100928e4dbabefcb64f1e71ecc
krutagya kaneria <krutagya.kaneria.cg@gmail.com>
2025-06-19 23:54:04 +0530

    Time: 1 ms (79.41%) | Memory: 10.1 MB (84.29%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -2,25 +2,26 @@ class Solution {
 public:
     int romanToInt(string s) {
         int total = 0;
-        int n = s.length();
-        
-        for (int i = 0; i < n; ++i) {
-            if (s[i] == 'I') {
-                if (i + 1 < n && (s[i + 1] == 'V' || s[i + 1] == 'X')) total -= 1;
-                else total += 1;
+        int prev = 0;
+
+        for (int i = s.size() - 1; i >= 0; --i) {
+            int curr = 0;
+            switch (s[i]) {
+                case 'I': curr = 1; break;
+                case 'V': curr = 5; break;
+                case 'X': curr = 10; break;
+                case 'L': curr = 50; break;
+                case 'C': curr = 100; break;
+                case 'D': curr = 500; break;
+                case 'M': curr = 1000; break;
             }
-            else if (s[i] == 'X') {
-                if (i + 1 < n && (s[i + 1] == 'L' || s[i + 1] == 'C')) total -= 10;
-                else total += 10;
-            }
-            else if (s[i] == 'C') {
-                if (i + 1 < n && (s[i + 1] == 'D' || s[i + 1] == 'M')) total -= 100;
-                else total += 100;
-            }
-            else if (s[i] == 'V') total += 5;
-            else if (s[i] == 'L') total += 50;
-            else if (s[i] == 'D') total += 500;
-            else if (s[i] == 'M') total += 1000;
+
+            if (curr < prev)
+                total -= curr;
+            else
+                total += curr;
+
+            prev = curr;
         }
 
         return total;
