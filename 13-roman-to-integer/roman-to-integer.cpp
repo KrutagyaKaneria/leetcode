fbefe0aed7fe707db3d8536f7be778a0ff621df6
krutagya kaneria <krutagya.kaneria.cg@gmail.com>
2025-06-19 23:53:21 +0530

    Time: 4 ms (64.76%) | Memory: 10.2 MB (74.90%) - LeetSync

--- 13-roman-to-integer/roman-to-integer.cpp
+++ 13-roman-to-integer/roman-to-integer.cpp
@@ -1,27 +1,26 @@
 class Solution {
 public:
     int romanToInt(string s) {
-        unordered_map<char, int> roman = {
-            {'I', 1},
-            {'V', 5},
-            {'X', 10},
-            {'L', 50},
-            {'C', 100},
-            {'D', 500},
-            {'M', 1000}
-        };
-
         int total = 0;
-        int prev = 0;
-
-        for (int i = s.size() - 1; i >= 0; --i) {
-            int curr = roman[s[i]];
-            if (curr < prev)
-                total -= curr;  // Subtract if smaller value before larger one
-            else
-                total += curr;  // Else add normally
-
-            prev = curr;
+        int n = s.length();
+        
+        for (int i = 0; i < n; ++i) {
+            if (s[i] == 'I') {
+                if (i + 1 < n && (s[i + 1] == 'V' || s[i + 1] == 'X')) total -= 1;
+                else total += 1;
+            }
+            else if (s[i] == 'X') {
+                if (i + 1 < n && (s[i + 1] == 'L' || s[i + 1] == 'C')) total -= 10;
+                else total += 10;
+            }
+            else if (s[i] == 'C') {
+                if (i + 1 < n && (s[i + 1] == 'D' || s[i + 1] == 'M')) total -= 100;
+                else total += 100;
+            }
+            else if (s[i] == 'V') total += 5;
+            else if (s[i] == 'L') total += 50;
+            else if (s[i] == 'D') total += 500;
+            else if (s[i] == 'M') total += 1000;
         }
 
         return total;
