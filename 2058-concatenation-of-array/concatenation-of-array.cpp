b420038358d456238d5304ff9b82ac9abf98336b
krutagya hirenbhai kaneria <krutagya.kaneria.cg@gmail.com>
2025-09-19 23:23:24 +0530

    Time: 0 ms (100.00%) | Memory: 17 MB (29.65%) - LeetSync

--- 2058-concatenation-of-array/concatenation-of-array.cpp
+++ 2058-concatenation-of-array/concatenation-of-array.cpp
@@ -2,9 +2,10 @@ class Solution {
 public:
     vector<int> getConcatenation(vector<int>& nums) {
         int n = nums.size();
-        for(int i=0;i<n;i++){
-            nums.push_back(nums[i]);
+        nums.resize(2 * n);  // increase size
+        for (int i = 0; i < n; i++) {
+            nums[i + n] = nums[i];  // put copy at the end
         }
-        return nums;
+        return nums;  // nums is now 2n long
     }
 };
