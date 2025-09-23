ba2883a3d8e81507fa22eefb50a74982e5d20df2
krutagya hirenbhai kaneria <krutagya.kaneria.cg@gmail.com>
2025-09-23 10:52:03 +0530

    Time: 2 ms (29.97%) | Memory: 23.1 MB (89.79%) - LeetSync

--- 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.cpp
+++ 3242-count-elements-with-maximum-frequency/count-elements-with-maximum-frequency.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
     int maxFrequencyElements(vector<int>& nums) {
-        vector<int> freq(101, 0); // if nums[i] is between 1 and 100
-        int maxFreq = 0;
+        map<int, int> freq;
+        int max = 0;
         int sum = 0;
-
-        // count frequencies and track max
         for (int c : nums) {
             freq[c]++;
-            if (freq[c] > maxFreq) {
-                maxFreq = freq[c];
+            if (freq[c] > max) {
+                max = freq[c];
             }
         }
 
-        // calculate sum
-        for (int f : freq) {
-            if (f == maxFreq) {
-                sum += f;
+        for (auto p : freq) {
+            if (p.second == max) {
+                sum += p.second;
             }
         }
 
