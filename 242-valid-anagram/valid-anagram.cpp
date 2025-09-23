353af583f6938ce60c6283e9b64af85e878d17a4
krutagya hirenbhai kaneria <krutagya.kaneria.cg@gmail.com>
2025-09-23 14:34:05 +0530

    Time: 7 ms (20.27%) | Memory: 9.8 MB (48.45%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,24 +1,21 @@
-#include <iostream>
-#include <vector>
-using namespace std;
-
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        if (s.length() != t.length()) return false;
-        
-        vector<int> count(26, 0); // count for 26 lowercase letters
-        
-        for (int i = 0; i < s.length(); ++i) {
-            count[s[i] - 'a']++;
-            count[t[i] - 'a']--;
-        }
-        
-        for (int c : count) {
-            if (c != 0) return false;
-        }
-        
-        return true;
+    if(s.length()!=t.length()){
+        return false;
+    }
+    map<char,int> freq;
+
+    for(char a : s){
+        freq[a]++;
+    }
+
+    for(char a : t){
+        freq[a]--;
+        if(freq[a] < 0) return false;
+    }
+    return true;
     }
 };
+
 
