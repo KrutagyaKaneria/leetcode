d61214acb07c9516397a84408b5e7011b82bd5ed
krutagya hirenbhai kaneria <krutagya.kaneria.cg@gmail.com>
2025-09-23 11:33:59 +0530

    Time: 0 ms (100.00%) | Memory: 8 MB (26.87%) - LeetSync

---
+++ 2491-smallest-even-multiple/smallest-even-multiple.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int smallestEvenMultiple(int n) {
+        int a =2;
+        int b=n;
+        int num1 = a;
+        int num2 = b;
+        while(a!=b) {
+            if(a>b){
+                a=a-b;
+            }
+            else{
+                b=b-a;
+            }
+        }
+        return (num1 * num2) / a;
+    }
+};
