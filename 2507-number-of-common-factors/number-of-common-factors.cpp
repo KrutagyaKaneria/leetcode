99096170a9fbedfaf11e3e52e803824be58ae3de
krutagya hirenbhai kaneria <krutagya.kaneria.cg@gmail.com>
2025-09-23 14:18:53 +0530

    Time: 2 ms (2.73%) | Memory: 8 MB (14.57%) - LeetSync

--- 2507-number-of-common-factors/number-of-common-factors.cpp
+++ 2507-number-of-common-factors/number-of-common-factors.cpp
@@ -1,12 +1,12 @@
 class Solution {
 public:
     int commonFactors(int a, int b) {
+        int cnt=0;
         int min;
         if(a>b){
             min=b;
         }
         min=a;
-        int cnt =0;
         for(int i=1;i<=min;i++){
             if(a%i==0 && b%i==0){
                 cnt++;
