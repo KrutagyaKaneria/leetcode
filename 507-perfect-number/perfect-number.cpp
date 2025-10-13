915fa58d0889db25158911640c7e2d7e821103c9
KRUTAGYA HIRENBHAI KANERIA <krutagya.kaneria.cg@gmail.com>
2025-10-13 18:43:04 +0530

    Time: 0 ms (100.00%) | Memory: 7.9 MB (4.98%) - LeetSync

--- 507-perfect-number/perfect-number.cpp
+++ 507-perfect-number/perfect-number.cpp
@@ -1,17 +1,16 @@
 class Solution {
 public:
     bool checkPerfectNumber(int num) {
-        int sum = 0;
-        for(int i=1;i<=num/2;i++){
-            if(num%i==0){
-                sum=sum+i;
+        if (num <= 1) return false;
+
+        int sum = 1;
+        for (int i = 2; i * i <= num; ++i) {
+            if (num % i == 0) {
+                sum += i;
+                if (i != num / i)
+                    sum += num / i;
             }
         }
-        if(sum==num){
-            return true;
-        }else{
-            return false;
-        }
-
+        return sum == num;
     }
 };
