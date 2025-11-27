5569796e1099203a3820534a7e95dc7451e360a9
KRUTAGYA HIRENBHAI KANERIA <krutagya.kaneria.cg@gmail.com>
2025-11-27 15:44:10 +0530

    Time: 2 ms (73.23%) | Memory: 14.8 MB (52.50%) - LeetSync

--- 1-two-sum/two-sum.cpp
+++ 1-two-sum/two-sum.cpp
@@ -1,13 +1,17 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int,int> map;
         int n = nums.size();
+
         for(int i=0;i<n;i++){
-            for(int j=i+1;j<n;j++){
-                if(nums[i] + nums[j] == target){
-                    return {i,j};
-                }
+            int x = target - nums[i];
+
+            if(map.find(x) != map.end()){
+                return{map[x],i};
             }
+
+            map[nums[i]] = i;
         }
         return {};
     }
