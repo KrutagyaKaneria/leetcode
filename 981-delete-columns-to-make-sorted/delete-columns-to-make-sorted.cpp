c713b9992b6c554f674fc532faee8931fcbf3a3f
KRUTAGYA HIRENBHAI KANERIA <krutagya.kaneria.cg@gmail.com>
2025-12-24 18:44:27 +0530

    Time: 3 ms (63.74%) | Memory: 15.7 MB (17.04%) - LeetSync

---
+++ 981-delete-columns-to-make-sorted/delete-columns-to-make-sorted.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int minDeletionSize(vector<string>& strs) {
+        int rows = strs.size();
+        int cols = strs[0].size();
+        int cnt = 0;
+
+        for(int i=0;i<cols;i++){
+            for(int j=0;j<rows-1;j++){
+                if(strs[j][i] > strs[j+1][i]){
+                    cnt++;
+                    break;
+                }
+            }
+        }
+        return cnt;
+    }
+};
