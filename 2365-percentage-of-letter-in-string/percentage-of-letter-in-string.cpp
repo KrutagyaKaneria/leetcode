76347b1235ecb4b549a0bf3362b982c48c1ae201
KRUTAGYA HIRENBHAI KANERIA <krutagya.kaneria.cg@gmail.com>
2026-01-03 15:07:30 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (68.38%) - LeetSync

---
+++ 2365-percentage-of-letter-in-string/percentage-of-letter-in-string.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int percentageLetter(string s, char letter) {
+        int size = s.length();
+        int count = 0;
+        for (char c : s) {
+            if (c == letter) {
+                count++;
+            }
+        }
+
+        return (count*100 / size);
+    }
+};
