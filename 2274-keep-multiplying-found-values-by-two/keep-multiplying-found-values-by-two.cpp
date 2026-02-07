510b91c3ccb78cc9ca50133a21a0d1f9aae89820
KRUTAGYA HIRENBHAI KANERIA <krutagya.kaneria.cg@gmail.com>
2026-02-08 01:01:05 +0530

    Time: 0 ms (100.00%) | Memory: 16 MB (6.78%) - LeetSync

---
+++ 2274-keep-multiplying-found-values-by-two/keep-multiplying-found-values-by-two.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int findFinalValue(std::vector<int>& nums, int n) {
+        unordered_set<int> numSet(nums.begin(), nums.end());
+        
+        while (numSet.count(n)){
+            n *= 2; 
+        }
+        
+        return n; 
+        }
+};
