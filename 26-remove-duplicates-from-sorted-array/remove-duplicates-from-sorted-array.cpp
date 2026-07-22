060445cf890180ec18d5a714881ef0f5ce720655
KRUTAGYA HIRENBHAI KANERIA <krutagya.kaneria.cg@gmail.com>
2026-07-22 23:00:02 +0530

    Time: 0 ms (100.00%) | Memory: 22.5 MB (98.29%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.cpp
@@ -1,15 +1,19 @@
 class Solution {
 public:
     int removeDuplicates(vector<int>& nums) {
-        if (nums.empty()) return 0; // Edge case: empty array
-        
-        int i = 0; // Pointer to track unique elements
-        for (int j = 1; j < nums.size(); j++) {
-            if (nums[j] != nums[i]) { // If a new unique element is found
-                i++;
-                nums[i] = nums[j]; // Move it to the correct position
+        int n = nums.size();
+        int cnt = 1;
+        int i = 1; 
+        int j = 1; 
+
+        while (i < n) { 
+            if (nums[i] != nums[i - 1]) {
+                nums[j] = nums[i];
+                cnt++;
+                j++; 
             }
+            i++; 
         }
-        return i + 1; // Length of the modified array
+        return cnt;
     }
 };
