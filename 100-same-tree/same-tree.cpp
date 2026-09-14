bb3e74de98bb45091a7871e56fef6d85c75f1d68
KRUTAGYA HIRENBHAI KANERIA <krutagya.kaneria.cg@gmail.com>
2026-09-14 17:28:33 +0530

    Time: 0 ms (100.00%) | Memory: 12.8 MB (47.29%) - LeetSync

--- 100-same-tree/same-tree.cpp
+++ 100-same-tree/same-tree.cpp
@@ -12,11 +12,13 @@
 class Solution {
 public:
     bool isSameTree(TreeNode* p, TreeNode* q) {
-        if(q==NULL || p==NULL){
-            return p==q;
+        if(!p && !q){
+            return true;
         }
-        int leftcount = isSameTree(q->left,p->left);
-        int rightcount = isSameTree(q->right,p->right);
-        return leftcount && rightcount && q->val == p->val;
+        if (!p || !q || p->val != q->val) {
+            return false;
+        }
+        return isSameTree(p->left,q->left) && isSameTree(p->right,q->right);
+
     }
 };
